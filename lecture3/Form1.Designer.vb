74c4ae139cf523a143209518426038b08f8e3d01
Danny Teel <themanofteel@icloud.com>
2021-04-14 14:26:40 -0400

    test average

---
+++ lecture3/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtExam1 = New System.Windows.Forms.TextBox()
+        Me.txtExam2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(267, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Exam 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(267, 282)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 32)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Exam 2"
+        '
+        'txtExam1
+        '
+        Me.txtExam1.Location = New System.Drawing.Point(721, 129)
+        Me.txtExam1.Name = "txtExam1"
+        Me.txtExam1.Size = New System.Drawing.Size(100, 38)
+        Me.txtExam1.TabIndex = 2
+        '
+        'txtExam2
+        '
+        Me.txtExam2.Location = New System.Drawing.Point(730, 276)
+        Me.txtExam2.Name = "txtExam2"
+        Me.txtExam2.Size = New System.Drawing.Size(100, 38)
+        Me.txtExam2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(194, 560)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(261, 156)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Add Exams"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(650, 560)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(301, 156)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Average"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(438, 861)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(307, 156)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 31
+        Me.ListBox1.Location = New System.Drawing.Point(298, 1065)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(577, 128)
+        Me.ListBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1972, 1221)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtExam2)
+        Me.Controls.Add(Me.txtExam1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtExam1 As TextBox
+    Friend WithEvents txtExam2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ lecture3/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    'declare yourexam 1 and 2 here
+    'as class level vars
+    'declare random obj here too
+
+    Dim exam1, exam2 As Integer
+    Dim rand As New Random
+
+    Private Sub txtExam1_TextChanged(sender As Object, e As EventArgs) Handles txtExam1.TextChanged
+        'generate a random exam1 score between 60 and 100
+        exam1 = rand.Next(60, 100)
+        exam2 = rand.Next(80, 100)
+        'write exam1 on txtExam1
+        txtExam1.Text = CStr(exam1)
+        txtExam2.Text = CStr(exam2)
+        'write the total on lblOutput
+        lblOutout.Text = "The total is " & (exam1 + exam2).ToString
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim average As Double
+        average = (exam1 + exam2) / 2
+        MessageBox.Show("The average is " & average.ToString)
+    End Sub
+End Class
